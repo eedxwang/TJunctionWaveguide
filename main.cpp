3ece53b4d1d3a36c0ae220b465eda1546420110c
Toftarn <carltoft@gmail.com>
2014-10-04 23:58:28 +0200

    Fixed race condition

--- main.cpp
+++ main.cpp
@@ -9,15 +9,6 @@
 // the terminal. Accepts two optional arguments: the number of 
 // basis functions and the number of waveguide modes to include
 
-/*int main()
-{
-	TJunction junction(5e9, 47.55e-3, 22.15e-3, 10e-3, 5, 100);
-	double* S = junction.SolveTJunction();
-	printf("S11: %f\nS22: %f\nS33: %f\n\n", S[0], S[1], S[2]);
-
-	return 0;
-}*/
-
 int main(int argc, char *argv[])
 {	
 	double a = 47.55e-3;
@@ -42,19 +33,25 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	printf("\nw/a \t\t S11 \t\t S21 \t\t S31\n\n");
-
+	double* S = new double[21 * 3];		// Will contain the three S-parameters for all 21 aperture widths
 	TJunction waveguide;
 	double start_time = (double)clock();
 	#pragma omp parallel for
 	for (int i = 0; i <= 20; i++)
 	{
 		// Calculate S-parameters and print them
-		double* SParameters = waveguide.SolveTJunction(f, a, b, a/20.0*i, basisFunctions, maxModeNumber);
-		printf("%f \t %f \t %f \t %f \n", 1.0/20.0*i, SParameters[0], SParameters[1], SParameters[2]);
+		double* SParameters = waveguide.SolveTJunction(f, a, b, a / 20.0*i, basisFunctions, maxModeNumber);
+		S[3 * i] = SParameters[0];
+		S[3 * i + 1] = SParameters[1];
+		S[3 * i + 2] = SParameters[2];
 	}
 	double end_time = (double)clock();
 	double time_lapsed = (end_time - start_time) / (double)CLOCKS_PER_SEC;
+
+	// Print the S-parameters
+	printf("\nw/a \t\t S11 \t\t S21 \t\t S31\n\n");
+	for (int i = 0; i <= 20; i++)
+		printf("%f \t %f \t %f \t %f \n", 1.0 / 20.0*i, S[3*i], S[3*i+1], S[3*i+2]);
 
 	printf("\nTime elapsed: %f s\n\n", time_lapsed);
 
